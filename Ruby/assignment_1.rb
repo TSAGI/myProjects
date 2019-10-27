9e3e73867a32dc0c5de00ee19284f5ea54f7c78f
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-10-26 23:32:36 -0400

    updated with regex

--- Ruby/assignment_1.rb
+++ Ruby/assignment_1.rb
@@ -1,23 +1,26 @@
 print "What was the student's score?: "
-score = gets.chomp.to_i
+score = gets.chomp
 
-while score < 0 || score > 100 
-    puts "The score must be between 0 and 100.\n"
-    print "Please input the student's score again: "
-    score = gets.chomp.to_i    
+while score =~ /[^0-9]+/
+    puts "The score must be an integer between 0 and 100. Try again."
+    print "What was the students score?: "
+    score = gets.chomp
+    if score =~ /^[0-9]+$/ then
+        score = score.to_i
+        if score > 89 && score < 101 then
+            puts "The student got an A+. Excellent work!!"
+        elsif score > 79 && score < 90 then
+            puts "The student got a A. Well done!!"
+        elsif score > 69 && score < 80 then
+            puts "The student got a B. Good job!!"
+        elsif score > 59 && score < 70 then
+            puts "The student got a C. Not Bad."
+        elsif score > 49 && score < 60 then
+            puts "The student got a D. Needs to study more."
+        elsif score >= 0  && score < 50 then
+            puts "The student failed. I'm sorry."
+        elsif score < 0 || score > 100 then
+            score = "apple"
+        end    
+    end
 end
-
-if score > 89 && score < 101 then
-    puts "The student got an A+. Excellent work!!"
-elsif score > 79 && score < 90 then
-    puts "The student got a A. Well done!!"
-elsif score > 69 && score < 80 then
-    puts "The student got a B. Good job!!"
-elsif score > 59 && score < 70 then
-    puts "The student got a C. Not Bad."
-elsif score > 49 && score < 60 then
-    puts "The student got a D. Needs to study more."
-elsif score < 50 then
-    puts "The student failed. I'm sorry."
-end
-
