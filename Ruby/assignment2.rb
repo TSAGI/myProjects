3260bfd6475b5452839b2888eb7879f8cb426ad9
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-10-29 22:54:09 -0400

    temperature conversion with error handling

--- Ruby/assignment2.rb
+++ Ruby/assignment2.rb
@@ -1,14 +1,35 @@
-print "Please enter the temperature: "
-fTemp = gets.chomp.to_f
-print "Is the number you entered in Fahrenheit or Celcius? (F or C): "
-f_or_c = gets.chomp
+fTemp = 0.0
+cTemp = 0.0
 
-if f_or_c.chars.first.downcase == "f" then
-    cTemp = (fTemp - 32) * (5.0/9)
-else
-    cTemp = fTemp
-    fTemp = (cTemp * (9/5.0)) + 32
-end
+loop do
+    print "Please enter the temperature: "
+    fTemp = gets.chomp
+    begin
+      if Integer(fTemp) 
+        fTemp = fTemp.to_f
+        loop do
+            print "Is the number you entered in Fahrenheit or Celcius? (F or C): "
+            f_or_c = gets.chomp.downcase
+            begin 
+                if f_or_c == "f" then
+                    cTemp = (fTemp - 32) * (5.0/9)
+                    break
+                elsif f_or_c == "c" then
+                    cTemp = fTemp
+                    fTemp = (cTemp * (9/5.0)) + 32
+                    break
+                else
+                    puts "Please press the letter F or C only."
+                end
+            end
+        end
+        break  
+      else 
+      end
+    rescue ArgumentError => _error
+        puts "You didn't put an number, please put a number and try again."
+    end   
+  end
 
 puts "The temperature in Fahrenheit is %.1f°F and the temperature in Celcius is %.1f°C"  % [fTemp, cTemp]
  
