6b7d5858e83b144163d486c94685028a7123c4d7
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-10-29 22:29:18 -0400

    fizzbuzz challenge

--- Ruby/fizzbuzz.rb
+++ Ruby/fizzbuzz.rb
@@ -1,23 +1,22 @@
 loop do
     print "Please enter a number: "
     fTemp = gets.chomp
+    testNum = fTemp
     begin
-      if Integer(fTemp) 
-        break  
-      end   
-    rescue ArgumentError => _error
-      puts "You didn't put an number, please put a number and try again."
-    end
-  end
-
-fTemp = fTemp.to_f
-
-if fTemp % 3 == 0 && fTemp % 5 == 0 then
-    puts "FIZZBUZZ"
-elsif fTemp % 3 == 0 
-    puts "FIZZ"
-elsif fTemp % 5 == 0 then
-    puts "BUZZ"
-else
-    "Sorry...no FIZZ or BUZZ for you."
+      if Integer(testNum)      
+        fTemp = fTemp.to_f        
+        if fTemp % 3 == 0 && fTemp % 5 == 0 then
+            puts "FIZZBUZZ"
+        elsif fTemp % 3 == 0 
+            puts "FIZZ"
+        elsif fTemp % 5 == 0 then
+            puts "BUZZ"
+        else
+            puts "Sorry...no FIZZ or BUZZ for you."
+        end        
+        break       
+    end   
+rescue ArgumentError => _error
+    puts "You didn't put an number, please put a number and try again."
+end
 end
