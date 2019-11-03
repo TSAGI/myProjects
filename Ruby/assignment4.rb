f7dc401b9c26b2ac19e933118d7e2d1eff3aa809
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-11-03 12:13:59 -0500

    while loops with and without regex

--- Ruby/assignment4.rb
+++ Ruby/assignment4.rb
@@ -1,6 +1,7 @@
 print "Enter your email address: "
 email = gets.chomp
-while email !~ /.*[@]/
+# while email !~ /.*[@]/
+while email.count("@") < 1
     puts "Your email address must contain an @ symbol. Try again."
     print "Enter your email address: "
     email = gets.chomp
@@ -8,7 +9,8 @@ end
 
 print "Enter your username (8 to 16 characters with no spaces): "
 username = gets.chomp
-while username !~ /^.{8,16}$/ || username !~ /^\S*$/
+#while username !~ /^.{8,16}$/ || username !~ /^\S*$/
+while username.length < 2 || username.length > 4 || username.match(" ")
     puts "Your username must contain between 8 to 16 characters with no spaces. Try again."
     print "Enter your username (8 to 16 characters with no spaces): "
     username = gets.chomp
@@ -16,7 +18,8 @@ end
 
 print "Enter your password (at least 12 characters and at least 1 uppercase letter): "
 password = gets.chomp
-while password !~ /^.*[A-Z]/ || password !~ /^.{12,}$/
+# while password !~ /^.*[A-Z]/ || password !~ /^.{12,}$/
+while password.count("A-Z") < 1 || password.length < 4
     puts "Your password must contain at least 12 characters and at least 1 uppercase letter: "
     print "Enter your email address: "
     password = gets.chomp
