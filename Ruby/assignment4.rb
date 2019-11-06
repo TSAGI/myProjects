88c69315c1425b8dcd6f8b813c4c8803be767a8b
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-11-05 21:02:34 -0500

    while loops and until loops

--- Ruby/assignment4.rb
+++ Ruby/assignment4.rb
@@ -6,22 +6,19 @@ until full_name.count(" ") > 0
     full_name = gets.chomp
 end
 
-confirm_email = "n"
-until confirm_email == "y"
+print "Enter your email address: "
+email = gets.chomp
+  while email.count("@") < 1
+    puts "Your email address must contain an @ symbol. Try again."
     print "Enter your email address: "
     email = gets.chomp
-    while email.count("@") < 1
-        puts "Your email address must contain an @ symbol. Try again."
-        print "Enter your email address: "
-        email = gets.chomp
-    end
-    print "Is #{email} your email address (Y or N): "
-    confirm_email = gets.chomp.downcase
-    while confirm_email != "y" && confirm_email != "n"
-        puts "Only Y or N are acceptable inputs. Try again."
-        print "Is #{email} your email address (Y or N): "
-        confirm_email = gets.chomp.downcase
-    end
+  end
+print "Please confirm your email address by entering it a second time: "
+confirm_email = gets.chomp
+until confirm_email == email
+    puts "Your emails don't match. Try again."
+    print "Please confirm your email address by entering it a second time: "
+    confirm_email = gets.chomp
 end
 
 print "Enter your username (8 to 16 characters with no spaces): "
@@ -36,6 +33,6 @@ print "Enter your password (at least 12 characters and at least 1 uppercase lett
 password = gets.chomp
 while password.count("A-Z") < 1 || password.length < 12
     puts "Your password must contain at least 12 characters and at least 1 uppercase letter: "
-    print "Enter your email address: "
+    print "Enter your password (at least 12 characters and at least 1 uppercase letter): "
     password = gets.chomp
 end
