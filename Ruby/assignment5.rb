e2b28825a2613dc9963403e82c678cc2b874538d
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-11-05 23:43:37 -0500

    while loops, until loops, and for loops

--- Ruby/assignment5.rb
+++ Ruby/assignment5.rb
@@ -1,38 +1,55 @@
+fields = []
+wrong_input = 0
 print "Enter your full name: "
-full_name = gets.chomp
-until full_name.count(" ") > 0
+fields[0] = gets.chomp
+until fields[0].count(" ") > 0
     puts "Your full name must contain at least 2 seperate names. Try again."
     print "Enter your full name: "
-    full_name = gets.chomp
+    fields[0] = gets.chomp
+    wrong_input += 1
 end
 
 print "Enter your email address: "
-email = gets.chomp
-  while email.count("@") < 1
+fields[1] = gets.chomp
+  while fields[1].count("@") < 1
     puts "Your email address must contain an @ symbol. Try again."
     print "Enter your email address: "
-    email = gets.chomp
+    fields[1] = gets.chomp
+    wrong_input += 1
   end
 print "Please confirm your email address by entering it a second time: "
 confirm_email = gets.chomp
-until confirm_email == email
+until confirm_email == fields[1]
     puts "Your emails don't match. Try again."
     print "Please confirm your email address by entering it a second time: "
     confirm_email = gets.chomp
+    wrong_input += 1
 end
 
 print "Enter your username (8 to 16 characters with no spaces): "
-username = gets.chomp
-while username.length < 8 || username.length > 16 || username.match(" ")
+fields[2] = gets.chomp
+while fields[2].length < 8 || fields[2].length > 16 || fields[2].match(" ")
     puts "Your username must contain between 8 to 16 characters with no spaces. Try again."
     print "Enter your username (8 to 16 characters with no spaces): "
-    username = gets.chomp
+    fields[2] = gets.chomp
+    wrong_input += 1
 end
 
 print "Enter your password (at least 12 characters and at least 1 uppercase letter): "
-password = gets.chomp
-while password.count("A-Z") < 1 || password.length < 12
+fields[3] = gets.chomp
+while fields[3].count("A-Z") < 1 || fields[3].length < 12
     puts "Your password must contain at least 12 characters and at least 1 uppercase letter: "
     print "Enter your password (at least 12 characters and at least 1 uppercase letter): "
-    password = gets.chomp
+    fields[3] = gets.chomp
+    wrong_input += 1
 end
+
+field_keys = ["Name", "Email", "Username", "Password" ]
+field_keys = field_keys.zip(fields)
+
+field_keys.each do |field_keys, fields|
+    puts "#{field_keys}: #{fields}"
+  end
+
+  puts "You entered the wrong input #{wrong_input} times."
+
