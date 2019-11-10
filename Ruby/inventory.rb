72069fc06c47e88da51076a71a6a2e75886eed7d
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-11-10 11:51:04 -0500

    more hash with items.key?(some_key)

--- Ruby/inventory.rb
+++ Ruby/inventory.rb
@@ -1,32 +1,38 @@
 def inventory
-print "How many items do you want to input?: "
-number_of_items = gets.chomp.to_i
+    print "How many items do you want to update?: "
+    number_of_items = gets.chomp.to_i
 
-counter = 1
-items = Hash.new
-item_name = ""
+    counter = 1
+    items = Hash.new
+    flag = 0
 
-while counter <= number_of_items do
-    for x in 1..number_of_items do
-        print "Enter the name of item ##{counter}: "
-        key = gets.chomp
-        if key == item_name then
-            puts "There were #{items[key]} #{key} in stock."
-            print "Enter the new amount of #{key} in stock: "
-            value = gets.chomp.to_i
-        else
-            item_name = key
-            items[key] = nil
-            print "Enter the amount of #{key} in stock: "
-            value = gets.chomp.to_i
+    while counter <= number_of_items do
+        for x in 1..number_of_items do
+            print "Enter the name of item ##{counter} to be updated: "
+            key = gets.chomp   
+            if items.key?(key) && flag == 1 then
+                puts "WARNING: YOU ALREADY UPDATED THIS ITEM."
+                puts "There were #{items[key]} #{key} in stock."
+            else     
+                if items.key?(key) then
+                    puts "There were #{items[key]} #{key} in stock."
+                    print "Enter the new amount of #{key} in stock: "
+                    value = gets.chomp.to_i
+                    flag = 1
+                else                
+                    items[key] = nil
+                    print "Enter the amount of #{key} in stock: "
+                    value = gets.chomp.to_i
+                end
+            end
+            items[key] = value
+            counter += 1
         end
-        items[key] = value
-        counter += 1
+    end
+
+    items.each do |key, val|
+        puts "#{key}: #{val}" 
     end
 end
 
-items.each do |key, val|
-    puts "#{key}: #{val}" 
-end
-end
 inventory
