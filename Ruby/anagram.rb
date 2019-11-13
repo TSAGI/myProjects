d47ecfd6862282525ae7b5b4f32120f95e00a0be
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-11-12 21:31:42 -0500

    method for comparing if 2 strings are anagrams

--- Ruby/anagram.rb
+++ Ruby/anagram.rb
@@ -5,9 +5,9 @@ def anagrams(s1, s2)
     string2 = s2.downcase.split("")
     string2.sort!
     if string1 == string2 then
-        puts "these 2 strings are anagrams"
+        return "these 2 strings are anagrams"
     else
-        puts "these 2 strings are NOT anagrams"
+        return "these 2 strings are NOT anagrams"
     end
 end
 
