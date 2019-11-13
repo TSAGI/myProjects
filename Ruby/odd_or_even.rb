d71f7e0d502dd7766f968f311d9357cb637e65c2
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-11-12 21:32:27 -0500

    method for checking if array is a majority of odd or even numbers

---
+++ Ruby/odd_or_even.rb
@@ -0,0 +1,18 @@
+def oddOrEven(arr)
+    even = 0
+    odd = 0
+    for x in arr
+        if x % 2 == 0 then
+            even  = even + 1
+        else
+            odd = odd + 1
+        end
+    end
+
+    if even > odd then
+        return "even"
+    else
+        return "odd"
+    end
+end
+
