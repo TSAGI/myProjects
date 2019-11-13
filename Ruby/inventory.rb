1b4a2fe90b165d646ca632267199d29b83a1b2b3
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-11-12 22:09:12 -0500

    a method that takes a hash as an argument, and updates it

--- Ruby/inventory.rb
+++ Ruby/inventory.rb
@@ -1,38 +1,44 @@
-def inventory
+hardware = Hash.new
+hardware = {
+    "computers" => 55,
+    "telephones" => 42,
+    "printers" => 14
+}
+
+
+def inventory(items)
     print "How many items do you want to update?: "
     number_of_items = gets.chomp.to_i
 
     counter = 1
-    items = Hash.new
-    flag = 0
-
+    entered_names = []
+    
     while counter <= number_of_items do
         for x in 1..number_of_items do
             print "Enter the name of item ##{counter} to be updated: "
-            key = gets.chomp   
-            if items.key?(key) && flag == 1 then
+            key = gets.chomp 
+            entered_names.push(key) 
+            if items.key?(key) && entered_names.count(key) > 1 then 
                 puts "WARNING: YOU ALREADY UPDATED THIS ITEM."
                 puts "There were #{items[key]} #{key} in stock."
-            else     
-                if items.key?(key) then
-                    puts "There were #{items[key]} #{key} in stock."
-                    print "Enter the new amount of #{key} in stock: "
-                    value = gets.chomp.to_i
-                    flag = 1
-                else                
-                    items[key] = nil
-                    print "Enter the amount of #{key} in stock: "
-                    value = gets.chomp.to_i
-                end
+            elsif items.key?(key) then
+                print "Enter the amount of #{key} in stock: "
+                value = gets.chomp.to_i    
+                items[key] = value              
+            else               
+                items[key] = nil
+                print "Enter the amount of #{key} in stock: "
+                value = gets.chomp.to_i   
+                items[key] = value                 
             end
-            items[key] = value
+            
             counter += 1
         end
     end
-
+    items = items.to_a.sort.to_h    
     items.each do |key, val|
         puts "#{key}: #{val}" 
     end
 end
 
-inventory
+inventory(hardware)
