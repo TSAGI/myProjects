9c4fb5836a3cf8b3a52abc875a907fd1b9450486
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-11-17 10:49:14 -0500

    method to check if the number of closing and opening brackets are equal, with some conditions

--- Ruby/string_validation.rb
+++ Ruby/string_validation.rb
@@ -4,13 +4,25 @@
 
 def validStr(s)
     string = s.split("")
+   
+    ob = 0
+    cb = 0
+    for x in string do
+        if x == "]"
+            cb += 1
+        elsif x == "["
+            ob += 1
+        end
+        if cb > ob
+            break
+        end
+    end
 
-    openBrac = string.count("[")
-    closBrac = string.count("]")
-
-    if (openBrac + closBrac) % 2 != 0 || string[0] == "]" then
+    if (ob + cb) % 2 != 0 then
         return "INVALID STRING"
     else
         return "VALID STRING"
     end
 end 
+
+
