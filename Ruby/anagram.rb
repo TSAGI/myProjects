a07b544a953d17de250d8d2c48b134dc46808235
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-11-17 12:06:53 -0500

    method for determining if 2 strings are anagrams

--- Ruby/anagram.rb
+++ Ruby/anagram.rb
@@ -1,9 +1,8 @@
 
 def anagrams(s1, s2)
-    string1 = s1.downcase.split("").sort
-    string1 = string1.delete(" ")
-    string2 = s2.downcase.split("").sort
-    string2 = string2.delete(" ")
+    string1 = s1.downcase.gsub(" ","").split("").sort
+    string2 = s2.downcase.gsub(" ","").split("").sort
+
     if string1 == string2 then
         return "these 2 strings are anagrams"
     else
