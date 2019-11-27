34f9d7776b8cded73d4a102df0898656ee9213ee
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-11-26 22:39:43 -0500

    iterating backwards and forwards to create various shapes

---
+++ Ruby/shapes.rb
@@ -0,0 +1,77 @@
+print "How many lines are in your rectangle?: "
+num = gets.chomp.to_i
+def rectangle(lines)
+for i in 1..lines    
+    for j in 1..lines
+        print "*"
+    end
+    puts
+end
+end
+rectangle(num)
+
+def halfPyramid(lines)    
+    for i in 1..lines    
+        print "*" * i 
+        puts         
+    end    
+end
+halfPyramid(num)
+
+def pyramid(lines)       
+    spaces = lines    
+    for i in 1..lines*2
+        if i % 2 != 0 then  
+            print " " * spaces  
+            print "*" * i            
+            puts  
+            spaces -= 1
+        end      
+    end
+end
+pyramid(num)
+
+def diamond(lines)
+    spaces = lines    
+    for i in 1..lines*2
+        if i % 2 != 0 then  
+            print " " * spaces  
+            print "*" * i            
+            puts  
+            spaces -= 1
+        end      
+    end
+    spaces = 2
+    ((lines-1)*2).downto(1) do |i|
+        if i % 2 != 0 then  
+            print " " * spaces  
+            print "*" * i            
+            puts  
+            spaces += 1
+        end      
+    end
+end
+diamond(num)
+
+def asterix(lines)
+    spaces = 1
+    (lines*2).downto(1) do |i|
+        if i % 2 != 0 then  
+            print " " * spaces  
+            print "*" * i            
+            puts  
+            spaces += 1
+        end      
+    end
+    spaces = lines
+    for i in 1..lines*2
+        if i % 2 != 0 then  
+            print " " * spaces  
+            print "*" * i            
+            puts  
+            spaces -= 1
+        end      
+    end
+
+end
+asterix(num)
