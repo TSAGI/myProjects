cebcdbeb16624c26c1f505456908128b2deea786
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-12-08 09:56:59 -0500

    hashes within hashes

---
+++ Ruby/hashes.rb
@@ -0,0 +1,13 @@
+clothing = {
+    "shoes" => {
+        "colors" => ["blue", "black", "white"],
+        "brands" => ["Nike", "New Balance", "Adidas"],
+        "sizes" => [8, 9, 10, 11]
+    }
+    "socks" => 
+    {
+        "colors" => ["blue", "black", "white"],
+        "brands" => ["Hanes", "Fruit of the Loom"],
+        "sizes" => ["small", "medium", "large"]
+    }
+}
