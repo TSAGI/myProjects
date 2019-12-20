9ef5331faad05a6c3e2da3a197a2b47e3ad7d037
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2019-12-19 22:05:38 -0500

    tic tac toe game

--- Ruby/tic_tac_toe.rb
+++ Ruby/tic_tac_toe.rb
@@ -16,25 +16,17 @@ until tic_tac_toe
     column_number = gets.chomp.to_i
     row[row_number][column_number] = "X"
     puts
+    
+    (1..3).each {|i| 
+        if row[i].count("X") == 3 ||
+            row[1][i] == "X" && row[2][i] == "X" && row[3][i] == "X"
+            tic_tac_toe = true 
+        end}   
 
-    if row[1].count("X") == 3 || row[2].count("X") == 3 || row[3].count("X") == 3
-       tic_tac_toe = true
-    end 
-    if row[1][1] == "X" && row[2][1] == "X" && row[3][1] == "X"
+    if row[1][1] == "X" && row[2][2] == "X" && row[3][3] == "X" ||
+        row[3][1] == "X" && row[2][2] == "X" && row[1][3] == "X"
         tic_tac_toe = true
-    end
-    if row[1][2] == "X" && row[2][2] == "X" && row[3][2] == "X"
-        tic_tac_toe = true
-    end
-    if row[1][3] == "X" && row[2][3] == "X" && row[3][3] == "X"
-        tic_tac_toe = true
-    end
-    if row[1][1] == "X" && row[2][2] == "X" && row[3][3] == "X"
-        tic_tac_toe = true
-    end
-    if row[3][1] == "X" && row[2][2] == "X" && row[1][3] == "X"
-        tic_tac_toe = true
-    end
+    end    
 end
 
 (0..3).each { |i|
