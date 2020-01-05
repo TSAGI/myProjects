935f316f0d840d2e676c22986d5bf0cc37626398
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2020-01-05 10:06:23 -0500

    linking files with different classes

--- Ruby/classes/bank.rb
+++ Ruby/classes/bank.rb
@@ -1,4 +1,6 @@
+
 class BankAccount
+    attr_accessor :id, :balance
 
     def initialize(id)
         @id = id
@@ -23,36 +25,3 @@ class BankAccount
 
 end
 
-class Branch
-    
-    def initialize
-        @accounts = []
-    end
-    
-    def add_account(account)
-        @accounts.push(account)
-    end
-
-    def deposit(account, amount)
-        account.add_funds(amount)
-    end
-    
-    def withdraw(account, amount)
-        account.subtract_funds(amount)
-    end
-    
-    def get_accounts
-        @accounts.each {|account| puts account.get_id}
-    end
-    
-end
-
-chequing = BankAccount.new(123)
-savings = BankAccount.new(345)
-
-rbc01 = Branch.new
-rbc01.add_account(chequing)
-rbc01.add_account(savings)
-rbc01.get_accounts
-rbc01.deposit(savings, 500)
-puts savings.get_balance
---
+++ Ruby/classes/branch.rb
@@ -0,0 +1,36 @@
+require_relative "bank.rb"
+
+class Branch
+    
+    def initialize
+        @accounts = []
+    end
+    
+    def add_account(account)
+        @accounts.push(account)
+    end
+
+    def deposit(account, amount)
+        account.add_funds(amount)
+    end
+    
+    def withdraw(account, amount)
+        account.subtract_funds(amount)
+    end
+    
+    def get_accounts
+        @accounts.each {|account| puts account.get_id}
+    end
+    
+end
+
+chequing = BankAccount.new(123)
+savings = BankAccount.new(345)
+
+rbc01 = Branch.new
+rbc01.add_account(chequing)
+rbc01.add_account(savings)
+rbc01.get_accounts
+rbc01.deposit(savings, 500)
+puts savings.get_balance
+puts savings.balance
